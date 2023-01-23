0a779156be0c65f2fb4d0cdcbfa5cb4ca1cc2130
luisedr98 <luisdominguez818@gmail.com>
2023-01-23 17:00:18 -0500

    add: enlace de github

--- src/App.jsx
+++ src/App.jsx
@@ -13,44 +13,36 @@ function App() {
   const [total, setTotal] = useState(0);
   const [pago, setPago] = useState(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     const totalPagar = calcularTotalPagar(parseInt(cantidad), parseInt(meses));
     setTotal(totalPagar);
-  }, [cantidad, meses])
+  }, [cantidad, meses]);
 
-  useEffect(()=>{
-    setPago((total/meses));
-  },[total])
+  useEffect(() => {
+    setPago(total / meses);
+  }, [total]);
 
   const handleChange = (e) => setCantidad(e.target.value);
 
-
   const handleOnclickIncremento = () => {
-      const valor = parseInt(cantidad) + step;
-      if (valor <= max) setCantidad(valor);
-      else alert('¡Valor máximo alcanzado!');
-  }
+    const valor = parseInt(cantidad) + step;
+    if (valor <= max) setCantidad(valor);
+    else alert("¡Valor máximo alcanzado!");
+  };
 
   const handleOnclickDecremento = () => {
     const valor = parseInt(cantidad) - step;
     if (valor >= min) setCantidad(valor);
-    else alert('¡Valor mínimo alcanzado!');;
-}
-
+    else alert("¡Valor mínimo alcanzado!");
+  };
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
       <div className="flex justify-between my-6">
-        <Button
-        operador="-"
-        fn={handleOnclickDecremento}
-        />
-        <Button
-        operador="+"
-        fn={handleOnclickIncremento}
-        />
+        <Button operador="-" fn={handleOnclickDecremento} />
+        <Button operador="+" fn={handleOnclickIncremento} />
       </div>
       <input
         type="range"
@@ -70,9 +62,11 @@ function App() {
         Elige un <span className="text-indigo-600">Plazo </span> a pagar
       </h2>
 
-      <select className="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
-              value={meses}
-              onChange={e => +setMeses(e.target.value)}>
+      <select
+        className="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={meses}
+        onChange={(e) => +setMeses(e.target.value)}
+      >
         <option value="6">6 Meses</option>
         <option value="12">12 Meses</option>
         <option value="24">24 Meses</option>
@@ -82,10 +76,31 @@ function App() {
         <h2 className="text-2xl font-extrabold text-gray-500 text-center">
           Resumen de <span className="text-indigo-600">Pago </span>
         </h2>
-        <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
-        <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total)} Total a pagar</p>
-        <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(pago)} Mensuales</p> 
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {meses} Meses
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {formatearDinero(total)} Total a pagar
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {formatearDinero(pago)} Mensuales
+        </p>
       </div>
+      
+      <p className="text-gray-500 text-center font-bold mb-5">Realizado por <span className="text-indigo-500 italic">Luis Dominguez</span></p>
+      
+      <div className="font-bold rounded flex justify-evenly items-center uppercase">
+      <div className="bg-indigo-600 hover:bg-indigo-800 w-5/12 mx-auto rounded">
+        <a class="text-white flex justify-center items-center" href="https://github.com/luisedr98">
+          <div>
+            <img src="./src/assets/github.svg" alt="imagen"/>
+          </div>
+          <p>github</p>
+        </a>
+      </div>   
+      </div>
+
+      
     </div>
   );
 }
